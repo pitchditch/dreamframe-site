06ba3e0e45c83dba65d6f9e23908cf95483afd25
pitchditch <jaydenf3800@gmail.com>
2026-08-19 00:42:20 -0700

    Route Virtual Estimates through working Vercel app
    * Force preview virtual estimates to Vercel
    
    * Send virtual estimates to working Vercel app

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { TranslationProvider } from "./hooks/use-translation.tsx";
 
 const queryClient = new QueryClient();
+const VIRTUAL_ESTIMATE_ORIGIN = "https://dreamframe-site.vercel.app";
 
 const hostname = window.location.hostname.toLowerCase();
 const pathname = window.location.pathname;
@@ -20,7 +21,7 @@ const isVirtualEstimateRoute =
 
 if (isLovablePreview && isVirtualEstimateRoute) {
   window.location.replace(
-    `https://bcpressurewashing.ca${window.location.pathname}${window.location.search}${window.location.hash}`,
+    `${VIRTUAL_ESTIMATE_ORIGIN}${window.location.pathname}${window.location.search}${window.location.hash}`,
   );
 } else {
   ReactDOM.createRoot(document.getElementById("root")!).render(
