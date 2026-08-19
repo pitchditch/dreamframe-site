83b8515c74e1967704d30bb99a98036284203b11
pitchditch <jaydenf3800@gmail.com>
2026-08-18 23:36:41 -0700

    Refresh crawler map after Google loads

--- src/components/house-tracking/MapComponent.ts
+++ src/components/house-tracking/MapComponent.ts
@@ -1,1 +1,30 @@
-export { default } from './MapComponentFixed';
+import React, { useEffect, useState } from 'react';
+import MapComponentFixed from './MapComponentFixed';
+
+const MapComponent: React.FC<React.ComponentProps<typeof MapComponentFixed>> = (props) => {
+  const [, setMapReadyTick] = useState(0);
+
+  useEffect(() => {
+    if ((window as any).google?.maps) {
+      setMapReadyTick((value) => value + 1);
+      return;
+    }
+
+    const interval = window.setInterval(() => {
+      if ((window as any).google?.maps) {
+        window.clearInterval(interval);
+        setMapReadyTick((value) => value + 1);
+      }
+    }, 100);
+
+    return () => window.clearInterval(interval);
+  }, []);
+
+  return React.createElement(MapComponentFixed, {
+    ...props,
+    pins: [...props.pins],
+    routes: [...props.routes],
+  });
+};
+
+export default MapComponent;
